84f0eb7471fab723e31f1615b051752e6ed7e9ec
vishal4765 <32396773+vishal4765@users.noreply.github.com>
2017-11-02 00:05:52 +0530

    Add files via upload

---
+++ admin_login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+ <html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Admin Login</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+<div class="container">
+	<section id="content">
+		<form action="">
+			<h1>SYSTEM ADMINISTRATOR</h1>
+			<div>
+				<input type="text" placeholder="Username" required="" id="username" />
+			</div>
+			<div>
+				<input type="password" placeholder="Password" required="" id="password" />
+			</div>
+			<div>
+				<input type="submit" value="Log in" />
+				<a href="#">Lost your password?</a>
+				
+			</div>
+		</form><!-- form -->
+		
+	</section><!-- content -->
+</div><!-- container -->
+</body>
+</html>
